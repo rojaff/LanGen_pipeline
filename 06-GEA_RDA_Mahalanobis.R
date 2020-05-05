f1fdb3a804bd60ea0ff68e3eff7018c35694cef7
Rodolfo Jaffé <r.jaffe@ib.usp.br>
2020-05-05 17:03:03 -0300

    Update and rename 6-GEA_RDA_Mahalanobis.R to 06-GEA_RDA_Mahalanobis.R

--- 06-GEA_RDA_Mahalanobis.R
+++ 06-GEA_RDA_Mahalanobis.R
@@ -1,11 +1,32 @@
-## This code was made to run RDA analysis to find loci potentially under selection
-## This script was developed by Brenna Forester and adapted to our data
+###############################################################################
+####################### VALE INSTITUTE OF TECHNOLOGY ##########################
+############### LABORATORIO DE GENETICA DA PAISAGEM - GENPAI ##################
+###############################################################################
 
+
+###############################################################################
+##### COMBINING GENOTYPE, PHENOTYPE, AND ENVIRONMENTAL DATA TO DELINEATE ######
+######  SITE-AJUSTED PROVENANCE STRATEGIES FOR ECOLOGICAL RESTORATION #########
+###############################################################################
+### AUTHORED BY: CAROLINA S. CARVALHO, BRENNA R. FORESTER, SIMONE K. MITRE, ###
+########## RONNIE ALVES, VERA L. IMPERATRIZ-FONSECA, SILVIO J. RAMOS, #########
+##### LUCIANA C. RESENDE-MOREIRA, JOSÉ 0. SIQUEIRA, LEONARDO C. TREVELIN, #####
+############# CECILIO F. CALDEIRA, MARKUS GASTAUER, RODOLFO JAFFÉ #############
+###############################################################################
+
+
+#------------------------------------------------------------------------------
+#                               PRE-ANALYSIS 
+#------------------------------------------------------------------------------
+
+##1. GOALS FOR THIS STEP:
+#A. PERFORM RDA ANALYSIS TO IDENTIFY LOCI UNDER SELECTION (OUTLIER DETECTION)
+
+##2. REMOVE ANY OBJECT OR FUNCTION IN THE ENVIRONMENT:
 rm(list=ls())
 
-# Load packages -----------------------------------------------------------
-
-library(vegan)    # Used to run RDA
+##3. INSTALL AND LOAD THE PACKAGES
+library(vegan)
 library(usdm)
 library(ade4)
 library(psych)
@@ -15,9 +36,13 @@ library(qvalue)
 library(r2vcftools)
 library(LEA)
 
-# Load functions ----------------------------------------------------------
+##4. INPUTS FOR THIS STEP:
+#A. THE FILE ".VCF" CLEANED AFTER FILTERING STEP 1.
+#B. DOWNLOAD A VCF FILE AS EXAMPLE "Icavalcantei.vcf" FROM FIGSHARE: https://doi.org/10.6084/m9.figshare.6100004.v1
+#C. CREATE A FOLDER NAMED "vcf" IN YOUR WORKING DIRECTORY AND SAVE THE .vcf FILE THERE.
+#D. ".CSV" FILE WITH GEOGRAPHICAL INFORMATION FOR THE GENETIC SAMPLES. YOU CAN DOWNLOAD IT THIS EXAMPLE IN https://www.frontiersin.org/articles/10.3389/fpls.2018.00532/full#supplementary-material
 
-### Summary stats
+##5. LOAD FUNCTIONS TO BE USED ON THIS STEP.
 VCFsummary <- function(snps){
   Nid <- nrow(snps@meta)
   Nsnps <- length(snps@site_id)
@@ -25,16 +50,17 @@ VCFsummary <- function(snps){
 }
 
 
-# Load SNPs and environmental dataset -------------------------------------
+#------------------------------------------------------------------------------
+#                         Load Imputed SNPs Datasets 
+#------------------------------------------------------------------------------
+## This script was developed by Brenna Forester and adapted to our data
 
-### Load snps data set to take the snps and individuals ID
-
+## Load snps data set to take the snps and individuals ID
 snps <-  vcfLink("vcf/ipomoea_filtered_within_ld_test2.vcf", overwriteID=T)
 snps@meta
 VCFsummary(snps) ## 115 individuals and 17025 SNPs.
 
-## read geno file
-
+## Read geno file
 gen.imp <- read.geno("vcf/ipomoea_filtered_within_ld_imputed.geno")
 gen.imp[1:10,1:10]
 colnames(gen.imp) <- snps@site_id
@@ -44,32 +70,35 @@ dim(gen.imp)
 ## Number of missing data
 sum(is.na(gen.imp))
 
-### load environmental data set
 
+#------------------------------------------------------------------------------
+#                          Load Environmental Dataset 
+#------------------------------------------------------------------------------
+## Load environmental data set
 dados_env <- read.table("adapt_var_mapping/Dados_ambientais/climatic_data_ipomoea.txt", head=T)
 head(dados_env)
 str(dados_env)
 
-# Confirm that genotypes and environmental data are in the same order
-
+## Confirm that genotypes and environmental data are in the same order
 data.frame(rownames(gen.imp),as.character(dados_env$ID))
 
-## check the correlation among enviromental variables (VIF < 3)
-
+## Check the correlation among enviromental variables (VIF < 3)
 vif(dados_env[,-c(1:3)])
 
-## scale environmental dataset
-
+## Scale environmental dataset
 pred_scale <- scale(dados_env[,-c(1:3)]) ## remove information of lat, long, location, etc
 rownames(pred_scale) <-dados_env[,1]
 head(pred_scale)
 
+
+#------------------------------------------------------------------------------
+#                             Perform RDA Analysis 
+#------------------------------------------------------------------------------
 #If you have more than one population, use the population assigned for each individual 
 #in the Condition argument of RDA to control for population structure
-#ex: m.rda <- rda(gen.imp ~ pred_scale + Condition(as.factor(pop_lea))) 
+# m.rda <- rda(gen.imp ~ pred_scale + Condition(as.factor(pop_lea))) 
 
 ## RDA with mahalanobis distance
-
 m.rda <- rda(gen.imp ~ pred_scale) 
 m.rda
 
@@ -79,55 +108,60 @@ plot(m.rda, scaling=3)
 
 summary(eigenvals(m.rda, model= "constrained"))
 
-#signif.axis <- anova(m.rda, by= "axis", nperm= 99) ## check axis significance
+## Check axis significance
+#signif.axis <- anova(m.rda, by= "axis", nperm= 99) 
 ## we chose the first 2 axes, which explained more than 80% of the variation
 
-## save loading of the first and second pcs
+## Save loading of the first and second pcs
 load.rda <- summary(m.rda)$species[,1:2]
 K <- 2  # the number of RDA axes you're looking at
 
-## estimate adjusted p-values based on gif
+
+#------------------------------------------------------------------------------
+#                             Outlier Detection 
+#------------------------------------------------------------------------------
+## Estimate adjusted p-values based on gif
 zscale <- apply(load.rda, 2, scale)        # I'm not sure scaling is the best approach here...
 mscale <- covRob(zscale, distance=TRUE, na.action=na.omit, estim="pairwiseGK")$dist
 (gif <- median(mscale)/qchisq(0.5, df=K)) #1.45
 
-## look at histogram
+## Look at histogram
 rda.pval <- pchisq(mscale/gif, df=K, lower.tail=FALSE)   # Remember: you can always change the GIF!!
 hist(rda.pval)
 
-## try with different gif if necessary
+## Try different gif values if necessary
 #rda.pval2 <- pchisq(mscale/1.1, df=K, lower.tail=FALSE)  # Less conservative GIF (smaller)
 #hist(rda.pval2)
 
-## select snps with FDR < 0.05
-
+## Select snps with FDR < 0.05
 rda.qval <- qvalue(rda.pval)$qvalues
 m.snps <- (rda.FDR <- colnames(gen.imp)[which(rda.qval < 0.05)])
 
 # Let's look at where these candidates are in the ordination space:
-
 snp.color <- as.data.frame(colnames(gen.imp), stringsAsFactors=F)
 snp.color[,2] <- apply(snp.color, 1, function(x) if(x %in% rda.FDR) 'gray32' else '#00000000')
 snp.color[,3] <- apply(snp.color, 1, function(x) if(x %in% rda.FDR) '#e31a1c' else '#00000000')
 
-# axes 1 & 2
+## Axes 1 & 2
 plot(m.rda, type="n", scaling=3, xlim=c(-1,1), ylim=c(-1,1))
 points(m.rda, display="species", pch=21, cex=1, col="gray32", bg='#f1eef6', scaling=3)
 points(m.rda, display="species", pch=21, cex=1, col=snp.color[,2], bg=snp.color[,3], scaling=3)
 text(m.rda, scaling=3, display="bp", col="#0868ac", cex=1)
 
-# axes 2 & 3
+## Axes 2 & 3
 plot(m.rda, type="n", scaling=3, xlim=c(-1,1), ylim=c(-1,1), choices=c(2,3))
 points(m.rda, display="species", pch=21, cex=1, col="gray32", bg='#f1eef6', scaling=3, choices=c(2,3))
 points(m.rda, display="species", pch=21, cex=1, col=snp.color[,2], bg=snp.color[,3], scaling=3, choices=c(2,3))
 text(m.rda, scaling=3, display="bp", col="#0868ac", cex=1, choices=c(2,3))
 
-## save the snps names to compare with other approaches
-
+## Save the snps names to compare with other approaches
 write.table(m.snps, "adapt_var_mapping/GEA-RDA-Mahalanobis/ipomoea_snps_mahalanobis_gea.txt")
 
+                       
+#------------------------------------------------------------------------------
+#        Correlation among Candidate SNPs and Environmental Predictors 
+#------------------------------------------------------------------------------                                   
 ## Add in the correlations of each candidate SNP with the environmental predictors:
-
 foo <- matrix(nrow=length(m.snps), ncol=3)  # 3 columns for 3 predictors
 colnames(foo) <- c("bio06","bio18", "bio16")
 
@@ -140,14 +174,11 @@ for (i in 1:length(m.snps)) {
 cand <- cbind.data.frame(m.snps,foo)  
 head(cand)
 
-## looking for duplicate detections
-
+## Looking for duplicate detections
 length(cand$m.snps[duplicated(cand$m.snps)])
-
 cand <- cand[!duplicated(cand$m.snps),] # remove duplicate detections
 
-##see which of the predictors each candidate SNP is most strongly correlated with
-
+## See which of the predictors each candidate SNP is most strongly correlated with
 for (i in 1:length(cand$m.snps)) {
   bar <- cand[i,]
   cand[i,5] <- names(which.max(abs(bar[2:4]))) # gives the variable
@@ -156,7 +187,6 @@ for (i in 1:length(cand$m.snps)) {
   
 }
 
-
 colnames(cand)[5] <- "predictor"
 colnames(cand)[6] <- "correlation"
 colnames(cand)[7] <- "correlation_real"
@@ -164,10 +194,8 @@ colnames(cand)[7] <- "correlation_real"
 table(cand$predictor) 
 head(cand)
 
-##Plot the SNPs
-
+## Plot the SNPs
 sel <- cand$m.snps
-
 env <- cand$predictor
 
 env[env=="bio06"] <- '#1f78b4'
@@ -175,7 +203,7 @@ env[env=="bio18"] <- '#e31a1c'
 env[env=="bio16"] <- '#b2df8a'
 
 
-# color by predictor:
+## Color by predictor:
 col.pred <- rownames(m.rda$CCA$v) # pull the SNP names
 
 for (i in 1:length(sel)) {           # color code candidate SNPs
@@ -189,17 +217,18 @@ empty[grep("#f1eef6",empty)] <- rgb(0,1,0, alpha=0) # transparent
 empty.outline <- ifelse(empty=="#00FF0000","#00FF0000","gray32")
 bg <- c('#1f78b4','#e31a1c','#b2df8a')
 
-# axes 1 & 2
+# Axes 1 & 2
 plot(m.rda, type="n", scaling=3, ylim=c(-1,1), xlim=c(-1,1))
 points(m.rda, display="species", pch=21, cex=1, col="gray32", bg=col.pred, scaling=3)
 points(m.rda, display="species", pch=21, cex=1, col=empty.outline, bg=empty, scaling=3)
 text(m.rda, scaling=3, display="bp", col="#0868ac", cex=1)
 legend("bottomright", legend=c("bio06","bio18","bio16"), bty="n", col="gray32", pch=21, cex=1, pt.bg=bg)
 
-# Save candidates loci and their fasta ------------------------------------
 
-### Load snps data set 
-
+#------------------------------------------------------------------------------
+#                     Save Candidates Loci and their Fasta
+#------------------------------------------------------------------------------                  
+## Load snps data set 
 snps <-  vcfLink("vcf/ipomoea_filtered_within_ld_test2.vcf", overwriteID=T)
 snps@meta
 VCFsummary(snps) ## 115 individuals and 17025 SNPs.
@@ -209,14 +238,14 @@ sel ##These are all the candidate SNPs. From here to FASTA files and BLAST
 snps_fil_rda_candidate <- Subset(snps, sites=as.character(sel))
 snps_fil_rda_candidate@site_id ##These are all the candidate SNPs. From here to FASTA files and BLAST
 
-#### Save filtered vcf
+## Save filtered vcf
 Save(snps_fil_rda_candidate, "adapt_var_mapping/GEA-RDA-Mahalanobis/snps_candidate_gea_RDA_mahalanobis_ipomoea.vcf")
 
-#### Retrieve chromosome IDS
+## Retrieve chromosome IDS
 snp_chrom <- Chrom(snps_fil_rda_candidate)
 CH1 <- unique(snp_chrom[, 1]) #225
 
-###### Retrieve sequences from fasta file 
+## Retrieve sequences from fasta file 
 fastafile <- read.fasta(file = "fasta/Icavalcantei.fasta", seqtype = "DNA", as.string = TRUE, set.attributes = FALSE)
 head(fastafile)
 length(fastafile)
@@ -224,8 +253,8 @@ length(fastafile)
 SEQ1 <- fastafile[names(fastafile) %in% CH1]
 length(SEQ1)
 
-### Save new fasta files containing candidate sequences
+## Save new fasta files containing candidate sequences
 
 seqinr::write.fasta(sequences = SEQ1, names = names(SEQ1), nbchar = 150, file.out = "adapt_var_mapping/GEA-RDA-Mahalanobis/SEQ_gea_RDA_mahalanobis_ipomoea.fasta")
 
-
+##END
